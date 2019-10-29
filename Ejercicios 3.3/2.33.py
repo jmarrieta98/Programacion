0f75f892c92c046d7ac5cf8cd2404768e280f45e
jmarrieta98 <jmarrieta98@gmail.com>
2019-10-29 10:11:10 +0100

    subir

--- Ejercicios 3.3/2.33.py
+++ Ejercicios 3.3/2.33.py
@@ -1,4 +1,6 @@
-#
+# Cuando el numero es divisible entre 100 o es divisible entre 4 y 100 imprime si 
+# en caso contrario imprime no
+# tambien hay un control de variable para que sea un numero lo que se introduce
 try:
     a=int(input("numero: "))
     if not a%400 or (not a%4 and a%100):
