35f252719c907780c0b629b29df3f02d57083449
jmarrieta98 <jmarrieta98@gmail.com>
2019-11-07 12:56:12 +0100

    subir

--- Ejercicios 3.1/2.22.py
+++ Ejercicios 3.1/2.22.py
@@ -1,4 +1,7 @@
+#Primero introducimos la fecha
+#Sumamos 1 al dia, si el dia es superior a los que tiene ese mes 
+# se pone a uno el dia y el mes se le suma 1, si el mes pasa de 12 se pone a uno y se le suma un año
+#Imprimimos la fecha
 from datetime import datetime, timedelta
-fecha = input('Ingrese fecha ==> "d/m/a": ')
-fecha = datetime.strptime(fecha, '%d/%m/%Y')
-print(fecha + timedelta(days=1))
+fecha = datetime.strptime(input('Ingrese fecha ==> "d/m/a": '), '%d/%m/%Y') + timedelta(days=1) 
+print(fecha.day,'/',fecha.month,'/',fecha.year, sep='')
