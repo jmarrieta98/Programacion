076850c0309be1701fcc67bd666ef9764e24ab98
jmarrieta98 <jmarrieta98@gmail.com>
2019-11-19 13:35:10 +0100

    subir

--- Codewars/ip.py
+++ Codewars/ip.py
@@ -1,11 +1,16 @@
 import sys
 try:
-    ip = sys.argv[1]
-    ip = ip.split(".")
+    ip = sys.argv[1].split(".")
 except:
     print("No has introducido nada")
 else:
-    if  len(ip)==4 and int(ip[0]) >=0 and int(ip[0]) <= 255 and int(ip[1]) >=0 and int(ip[1]) <= 255 and int(ip[2]) >=0 and int(ip[2]) <= 255 and int(ip[3]) >=0 and int(ip[3]) <= 255:
-            print("Correcto")
+    if  len(ip)==4:
+        try:
+            if int(ip[0]) >=0 and int(ip[0]) <= 255 and int(ip[1]) >=0 and int(ip[1]) <= 255 and int(ip[2]) >=0 and int(ip[2]) <= 255 and int(ip[3]) >=0 and int(ip[3]) <= 255:
+                print("Correcto")
+            else:
+                print("Valores fuera de rango")
+        except:
+            print("Has introducido letras o en blanco")
     else:
-        print("Incorrecto")
+        print("Longitud incorrecta")
