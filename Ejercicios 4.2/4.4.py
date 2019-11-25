dd5e5a80beb403aa233f6a194987dad1670292fc
jmarrieta98 <jmarrieta98@gmail.com>
2019-11-25 15:07:15 +0100

    subir

---
+++ Ejercicios 4.2/4.4.py
@@ -0,0 +1,16 @@
+def eliminar_extremos(lista):
+    lista.remove(lista[0])
+    lista.remove(lista[len(lista)-1])
+    return lista
+
+def valoracion(lista):
+    media = 0
+    lista=eliminar_extremos(sorted(lista))
+    print(lista)
+    for i in lista:
+        media+=i
+    return media/len(lista)
+
+if __name__=="__main__":
+    lista=[3,2,1,4,8,7,6,5]
+    print(valoracion(lista))
