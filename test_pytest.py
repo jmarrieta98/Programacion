3c755aad992efce79fd17bfc931512093133563f
jmarrieta98 <jmarrieta98@gmail.com>
2019-12-03 12:32:02 +0100

    subir

--- test_pytest.py
+++ test_pytest.py
@@ -1,7 +1,8 @@
 from Codewars import raizdoble    # The code to test
 
+#Run Test|Debug Test
 def t_raiz():
     assert raizdoble.raiz(16)==4
-
+#Run Test|Debug Test
 def t_cuadrado():
     assert raizdoble.cuadrado(4)==16
