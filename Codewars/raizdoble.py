904aa1ace260390449ced97f0e3e05d3188a91bd
jmarrieta98 <jmarrieta98@gmail.com>
2019-12-03 12:06:44 +0100

    subir

--- Codewars/raizdoble.py
+++ Codewars/raizdoble.py
@@ -6,7 +6,7 @@ def raiz (x):
         r = 0.5 * ( (x/r)+r)
     return r
 def cuadrado(x):
-    return x**2
+    return x*x
 
 if __name__ == '__main__':
     print(raiz(int(input("Introduce un numero:\t"))))
---
+++ test_pytest.py
@@ -0,0 +1,7 @@
+from Codewars import raizdoble    # The code to test
+
+def t_raiz():
+    assert raizdoble.raiz(16)==4
+
+def t_cuadrado():
+    assert raizdoble.cuadrado(4)==16
