c799f6f013681151c1ae4d8b96a0365213afa67b
jmarrieta98 <jmarrieta98@gmail.com>
2019-12-05 22:25:46 +0100

    subir

---
+++ Ejercicios 4.3/destruirlista.py
@@ -0,0 +1,10 @@
+equipos = {
+    'CHB' : ['chiclana blues','chiclana','blues','chb'],
+    'CAW' : ['cádiz wheels','cádiz','wheels','caw'],
+    'CSU' : ['caleta surfers','celta','surfers','csu'],
+    'COS' : ['conil suns','conil','suns','cos'],
+    'VIB' : ['victoria bedouins','victoria','bedouins','vib'],
+    'CHE' : ['cortadura hearts','cortadura','hearts','che']
+}
+equipos=list(equipos.items())
+print(equipos)
