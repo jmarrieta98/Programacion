be95aaf76d15dceb2709d814c2d7de36b45f7ea6
jmarrieta98 <jmarrieta98@gmail.com>
2020-01-08 08:45:36 +0100

    subir

---
+++ Ejercicios 5.2/5.3.py
@@ -0,0 +1,17 @@
+diccionario={
+
+}
+
+if __name__ == "__main__":    
+    with open('archivo2.csv') as f: 
+        lis = [x.split() for x in f] 
+    claves = lis.pop(0)
+    print(claves)
+    
+    for i in claves:
+        diccionario[i] = []
+    
+    for i in lis:
+        for j in range(len(i)):
+            diccionario[claves[j]].append(i[j])
+    print(diccionario)
